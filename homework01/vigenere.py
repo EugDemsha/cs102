77a5826f5d3f2b02f1d657a151340f4c07c6e40c
EugDemsha <eug.demsha@gmail.com>
2021-10-15 18:39:41 +0300

    Реализованы функции encrypt_vigenere и decrypt_vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -11,6 +11,30 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+    if len(keyword) < len(plaintext):
+        for i in keyword:
+            if len(keyword) < len(plaintext):
+                keyword += i
+        return encrypt_vigenere(plaintext, keyword)
+    else:
+        for a in range(len(plaintext)):
+            b = plaintext[a]
+            c = keyword[a]
+            if c.istitle():
+                shift = ord(c) - ord("A")
+            else:
+                shift = ord(c) - ord("a")
+            if b.isalpha():
+                if b.istitle():
+                    b = chr(ord(b) + shift)
+                    if ord(b) > ord("Z"):
+                        b = chr(ord(b) - 26)
+                else:
+                    b = chr(ord(b) + shift)
+                    if ord(b) > ord("z"):
+                        b = chr(ord(b) - 26)
+            ciphertext += b
+
     return ciphertext
 
 
@@ -27,4 +51,28 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+    if len(keyword) < len(ciphertext):
+        for i in keyword:
+            if len(keyword) < len(ciphertext):
+                keyword += i
+        return decrypt_vigenere(ciphertext, keyword)
+    else:
+        for a in range(len(ciphertext)):
+            b = ciphertext[a]
+            c = keyword[a]
+            if c.istitle():
+                shift = ord(c) - ord("A")
+            else:
+                shift = ord(c) - ord("a")
+            if b.isalpha():
+                if b.istitle():
+                    b = chr(ord(b) - shift)
+                    if ord(b) < ord("A"):
+                        b = chr(ord(b) + 26)
+                else:
+                    b = chr(ord(b) - shift)
+                    if ord(b) < ord("a"):
+                        b = chr(ord(b) + 26)
+            plaintext += b
+
     return plaintext
