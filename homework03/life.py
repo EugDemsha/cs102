2d92a3bbf186a4c7f215127c4d0baff7cad1ee1c
EugDemsha <eug.demsha@gmail.com>
2022-01-10 15:07:51 +0300

    Try to fix mypy

--- homework03/life.py
+++ homework03/life.py
@@ -82,7 +82,7 @@ class GameOfLife:
         """
         Не превысило ли текущее число поколений максимально допустимое.
         """
-        if self.generations >= self.max_generations:
+        if self.max_generations and self.generations >= self.max_generations:
             return True
         return False
 
