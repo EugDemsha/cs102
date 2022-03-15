ffd29dccebb88ea6e3e514af9f566b22e811c215
EugDemsha <eug.demsha@gmail.com>
2022-03-15 12:51:17 +0300

    Here we go again

--- homework05/vkapi/friends.py
+++ homework05/vkapi/friends.py
@@ -83,7 +83,7 @@ def get_mutual(
         tries = ((len(target_uids) - 1) // 100) + 1  # type: ignore
         for i in range(tries):
             try:
-                url = f"friends.getMutual?access_token={access_token}&source_uid={source_uid}&target_uid={target_uid}&target_uids={','.join(list(map(str, target_uids)))}&count={count}&offset={i*100}&v={v}"  # type: ignore
+                url = f"friends.getMutual?access_token={access_token}&source_uid={source_uid}&target_uid={target_uid}&target_uids={','.join([str(t) for t in target_uids])}&count={count}&offset={i*100}&v={v}"
                 response = s.get(url)
                 for friend in response.json()["response"]:
                     active.append(
