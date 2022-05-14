5f2bcdb3fac5091c9e22a5033cf2d836d777e8e5
EugDemsha <eug.demsha@gmail.com>
2022-05-15 02:20:36 +0300

    hacker

--- homework06/scraputils.py
+++ homework06/scraputils.py
@@ -4,16 +4,36 @@ from bs4 import BeautifulSoup
 
 def extract_news(parser):
     """ Extract news from a given web page """
+
     news_list = []
 
-    # PUT YOUR CODE HERE
+    athing = parser.find_all(class_="athing")
+    subtext = parser.find_all(class_="subtext")
+
+    for i in range(len(athing)):
+        author = subtext[i].find(class_="hnuser")
+        comments = subtext[i].find_all("a")[-1].text[:-9]
+        points = subtext[i].find(class_="score")
+        title = athing[i].find(class_="titlelink").text
+        url = athing[i].find(class_="titlelink")["href"]
+        news_list.append(
+            {
+                "author": author.text if author else "None",
+                "comments": int(comments) if comments else 0,
+                "points": int(points.string.split()[0]) if points else 0,
+                "title": title,
+                "url": url if "http" in url else "https://news.ycombinator.com/" + url,
+            }
+        )
 
     return news_list
 
 
 def extract_next_page(parser):
     """ Extract next page URL """
-    # PUT YOUR CODE HERE
+    if not parser.find(class_="morelink"):
+        return None
+    return parser.find(class_="morelink")["href"]
 
 
 def get_news(url, n_pages=1):
@@ -25,8 +45,9 @@ def get_news(url, n_pages=1):
         soup = BeautifulSoup(response.text, "html.parser")
         news_list = extract_news(soup)
         next_page = extract_next_page(soup)
+        if not next_page:
+            next_page = url[29:36] + str(int(url[36:]) + 1)
         url = "https://news.ycombinator.com/" + next_page
         news.extend(news_list)
         n_pages -= 1
     return news
-
